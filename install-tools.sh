fdfbbc979a228d613fb99509793250c3e7489fe2
Rob Gonnella <robgonnella@gmail.com>
2020-12-07 12:08:52 -0500

    Simplify tooling
    Install fewer tools and instead focus on just setting up basic config

--- install-tools.sh
+++ install-tools.sh
@@ -38,28 +38,6 @@ install_zsh() {
   fi
 }
 
-install_spacemacs() {
-  if ! emacs --version > /dev/null 2>&1; then
-    echo "Installing emacs"
-    if [ "$PLATFORM" = "Darwin" ]; then
-        brew tap d12frosted/emacs-plus
-        install_package emacs-plus 2>&1
-    else
-        sudo add-apt-repository ppa:kelleyk/emacs
-        install_package emacs26
-    fi
-    mv ~/.emacs.d ~/.emacs.d.bak
-    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-    git -C ~/.emacs.d checkout develop
-  else
-    echo "Skipping emacs installation: Already installed"
-  fi
-  if ! which ispell > /dev/null; then
-    echo "Installing ispell"
-    get_install_package ispell
-  fi
-}
-
 install_git() {
   if ! git --version > /dev/null 2>&1; then
     echo "Installing git"
@@ -84,90 +62,6 @@ install_vim() {
     ln -s ~/.vim/apprentice/colors/apprentice.vim ~/.vim/colors/apprentice.vim
   else
     echo "Skipping vim installation: Already installed"
-  fi
-}
-
-install_go() {
-  if ! go version > /dev/null 2>&1; then
-    echo "Installing golang"
-    if [ "$PLATFORM" = "Darwin" ]; then
-        install_package go
-    else
-      sudo add-apt-repository ppa:longsleep/golang-backports
-      install_package golang-go
-    fi
-    mkdir -p ~/code/go/src ~/code/go/pkg ~/code/go/bin
-  else
-    echo "Skipping go installation: Already installed"
-  fi
-  GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
-  GO111MODULE=on CGO_ENABLED=0 go get \
-    -v \
-    -trimpath \
-    -ldflags '-s -w' \
-    github.com/golangci/golangci-lint/cmd/golangci-lint
-}
-
-install_node() {
-  if [ ! -d ~/.nvm ]; then
-    echo "Installing nvm"
-    if [ "$PLATFORM" = "Linux" ]; then
-      install_package curl
-    fi
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-    sudo chown -R $USER ~/.nvm
-  else
-    echo "Skipping nvm installation: Already installed"
-  fi
-  if ! node --version > /dev/null 2>&1; then
-    . ~/.nvm/nvm.sh
-    nvm install node
-  else
-    echo "Skipping node installation: Already installed"
-  fi
-}
-
-install_yarn() {
-  if ! yarn --version > /dev/null; then
-    echo "Installiing yarn"
-    if [ "$PLATFORM" = "Darwin" ]; then
-      install_package yarn --ignore-dependencies
-    else
-      curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-      echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-      install_package --no-install-recommends yarn
-    fi
-  else
-    echo "Skipping yarn installation: Already installed"
-  fi
-}
-
-install_rbenv() {
-  if ! rbenv --version > /dev/null 2>&1; then
-    echo "Installing rbenv"
-    if [ "$PLATFORM" = "Darwin" ]; then
-      install_package rbenv
-    else
-      install_package \
-        autoconf \
-        bison \
-        build-essential \
-        libssl-dev \
-        libyaml-dev \
-        libreadline6-dev \
-        zlib1g-dev \
-        libncurses5-dev \
-        libffi-dev \
-        libgdbm5 \
-        libgdbm-dev
-      git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-      # install ruby-build as plugin for installing ruby versions
-      mkdir -p ~/.rbenv/plugins
-      git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-      sudo chown -R $USER ~/.rbenv
-    fi
-  else
-    echo "Skipping rbenv installation: Already installed"
   fi
 }
 
@@ -228,15 +122,9 @@ install_homebrew
 install_git
 install_zsh
 install_vim
-install_go
-install_node
-install_yarn
-install_rbenv
 install_docker
 install_powerline_fonts
 install_oh_my_zsh
-install_spacemacs
-
 echo "***************************"
 echo "Installation complete. You must log out and log back in for changes to take effect"
 echo "***************************"
