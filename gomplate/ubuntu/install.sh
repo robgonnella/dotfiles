19d89e65e922d263bdc50624b3a81a3900e4b74b
Rob Gonnella <robgonnella@gmail.com>
2022-06-12 15:17:08 -0400

    Minor updates to get ubuntu scripts to work

--- gomplate/ubuntu/install.sh
+++ gomplate/ubuntu/install.sh
@@ -3,7 +3,9 @@
 if ! gomplate -v >/dev/null 2>&1; then
   echo "installing gomplate"
   sudo apt update && sudo apt install -y jq curl ca-certificates
-  local url=$(curl -Lo /usr/local/bin/gomplate https://api.github.com/repos/hairyhenderson/gomplate/releases/latest | jq '.assets | .[] | select(.name=="gomplate_linux-amd64") | .browser_download_url')
+  url=$(curl https://api.github.com/repos/hairyhenderson/gomplate/releases/latest | jq '.assets | .[] | select(.name=="gomplate_linux-amd64") | .browser_download_url')
+  url="${url//\"/}"
+  sudo curl -Lo /usr/local/bin/gomplate $url
   sudo chmod +x /usr/local/bin/gomplate
 else
   echo "gomplate already installed. Skipping."
--- pyenv/ubuntu/install.sh
+++ pyenv/ubuntu/install.sh
@@ -6,23 +6,28 @@ HERE=$(cd $(dirname "${BASH_SOURCE[0]}") >/dev/null 2>&1 && pwd)
 
 source $HERE/../common.sh
 
-sudo apt update && sudo apt install -y make \
-  build-essential \
-  libssl-dev \
-  zlib1g-dev \
-  libbz2-dev \
-  libreadline-dev \
-  libsqlite3-dev \
-  wget \
-  curl \
-  llvm \
-  libncursesw5-dev \
-  xz-utils \
-  tk-dev \
-  libxml2-dev \
-  libxmlsec1-dev \
-  libffi-dev \
-  liblzma-dev
+if ! pyenv --version >/dev/null 2>&1; then
+  echo "Installing pyenv"
+  sudo apt update && sudo apt install -y make \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
 
 
-install_pyenv
+  install_pyenv
+else
+  echo "pyenv already installed. Skipping."
+fi
