f03a13958930b909ce1f5a6ae85683952219a0bc
Rob Gonnella <rob.gonnella@papayapay.com>
2024-11-08 15:52:23 -0500

    Consolidate scripts

--- .chezmoiscripts/run_once_before_01_install-homebrew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-if ! command -v brew &> /dev/null; then
-  echo "===> Installing Homebrew (may prompt for password)"
-
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
--- .chezmoiscripts/run_onchange_after_03_add-poetry-plugin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -d $HOME/.oh-my-zsh/plugins/poetry ]; then
-    mkdir $HOME/.oh-my-zsh/plugins/poetry
-    poetry completions zsh > $HOME/.oh-my-zsh/plugins/poetry/_poetry
-fi
